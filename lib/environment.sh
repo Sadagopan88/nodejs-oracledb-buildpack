d37b102084b107b63902a0454f255ff1011dd74e
Kommavarapu <Lakshmi.Kommavarapu@aig.com>
2017-11-07 11:32:22 -0600

    commit export GIT_SSL_NO_VERIFY=1

--- lib/environment.sh
+++ lib/environment.sh
@@ -11,6 +11,7 @@ add_oracle_env(){
     export LD_LIBRARY_PATH=$build_dir/oracle/instantclient:${LD_LIBRARY_PATH:-}
     export OCI_LIB_DIR=$build_dir/oracle/instantclient
     export OCI_INC_DIR=$build_dir/oracle/instantclient/sdk/include
+    export GIT_SSL_NO_VERIFY=1
     echo "build directory = $build_dir"
     echo "----/Add Oracle env variables----"
     echo "build directory = $build_dir"
